dfd39c20603761ad5c58a6b91bf036f98a3b9737
Ovidiu-Mihai <ovidiumihaibelciug@gmail.com>
2017-11-12 14:21:54 +0200

    Profile UI

---
+++ resources/assets/js/_/components/includes/dumb/Curs.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <div class="card cursCard">
+            <div class="card-image">
+                <figure class="image is-2by1" style="background:url(http://www.codecaven.co.uk/images/laravel.jpg); background-position: center">
+
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-left">
+                        <figure class="image is-48x48">
+                            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                        </figure>
+                    </div>
+                    <div class="media-content">
+                        <p class="title is-4">John Smith</p>
+                        <p class="subtitle is-6">@johnsmith</p>
+                    </div>
+                </div>
+
+                <div class="content">
+                    adasdasd
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
+                    <br><br>
+                    <progress class="progress is-primary" value="15" max="100">30%</progress>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+
+    }
+
+</script>
---
+++ resources/assets/js/_/components/includes/dumb/Item.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <div class="tooltipCustom">
+        <div class="item-box">
+            <div class="customBox">aasd</div>
+        </div>
+        <span class="tooltiptext">loremadakj nsdkandk naskdn askndkas
+              da sdkaksdasnd
+              sdnas
+            </span>
+    </div>
+
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
